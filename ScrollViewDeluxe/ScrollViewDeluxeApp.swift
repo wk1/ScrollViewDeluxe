0bd1cd0257489c518fc2765bde855426ab63a6f0
Christian Hoock <9932295+wk1@users.noreply.github.com>
2023-03-02 16:27:16 +0100

    Initial Commit

---
+++ ScrollViewDeluxe/ScrollViewDeluxeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewDeluxeApp.swift
+//  ScrollViewDeluxe
+//
+//  Created by Christian Hoock on 02.03.23.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewDeluxeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
